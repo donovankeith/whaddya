f002c2cfca417b435caf6b6f44a10d9a50ac74fc
Donovan Keith <donovanskeith@gmail.com>
2015-06-23 15:50:06 -0700

    Switched to a MongoDB website.

--- you-should.js
+++ you-should.js
@@ -1,10 +1,11 @@
+Activities = new Mongo.Collection("activities");
+
 if (Meteor.isClient) {
-  // This code only runs on the client
-  Template.body.helpers({
-    activities: [
-      { title: "See a show at UCB." },
-      { title: "Go to dinner at that cool bar in WeHo you walked past." },
-      { title: "Attend a random meetup." }
-    ]
-  });
+    //This code only runs on the client.
+
+    Template.body.helpers({
+        activities: function () {
+            return Activities.find({});
+        }
+    });
 }
